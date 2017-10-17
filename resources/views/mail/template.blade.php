65dba3abf30ca2a688718c4e3199569a8201a801
Alex Roye <alex@creativefaze.com>
2017-10-17 09:32:15 +0500

    Added number and address comlumns to users table and mail template

--- resources/views/mail/template.blade.php
+++ resources/views/mail/template.blade.php
@@ -1,1 +1,1 @@
-<p>You received this mail from Daily Classifieds, to let you know that <a href="{{ $link }}" target="_blank">your ad</a> has been approved by our admin.</p>
+<p>You received this mail from Daily Classifieds, to let you know that your ad has been approved by our admin.</p>
