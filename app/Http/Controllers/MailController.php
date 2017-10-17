e1abdd6609e04d4602635eed0b0af399fac5e0cb
Alex Roye <alex@creativefaze.com>
2017-10-17 09:30:09 +0500

    Separate controller to handle sitewide mails

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -12,13 +12,13 @@ class MailController extends Controller
 {
   public function sendMail(MailRequest $request){
     $data = array(
-      'from'=>$request->input('from'),
-      'subject'=>$request->input('subject'),
-      'mailText'=>$request->input('message'),
+      'from'=>$request->get('from'),
+      'subject'=>$request->get('subject'),
+      'mailText'=>$request->get('message'),
     );
     $send = Mail::send(['html' => 'mail.template'], $data, function($message)use($request){
-      $message->from('ahmed.raza@square63.com', 'Daily Classifieds Contact Submission');
-      $message->to('raza1778@gmail.com')->subject($request->input('subject'));
+      $message->from('ahmed@creativefaze.com', 'Daily Classifieds Contact Submission');
+      $message->to('raza1778@gmail.com')->subject('Test');
     });
     if ($send) {
       return redirect('contact')->with('message', 'Message sent to our admins.');
